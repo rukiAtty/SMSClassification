8cc6171a951d06f8e953b58b36c04141bd1e4c67
rukiAtty <rukshanaattygalle@gmail.com>
2023-10-29 01:31:16 +0530

    working application

--- myenvr/Library/bin/c_rehash.pl
+++ myenvr/Library/bin/c_rehash.pl
@@ -13,7 +13,7 @@
 # and add symbolic links to their hash values.
 
 my $dir = "C:\\Program Files\\Common Files\\ssl";
-my $prefix = "C:/Users/rla/OneDrive - Tiqri Corporation Pte Ltd/Desktop/TextractApp/sms+spam+collection/myenv\\Library";
+my $prefix = "C:/Users/rla/OneDrive - Tiqri Corporation Pte Ltd/Desktop/TextractApp/SMSClassification/myenvr\\Library";
 
 my $errorcount = 0;
 my $openssl = $ENV{OPENSSL} || "openssl";
